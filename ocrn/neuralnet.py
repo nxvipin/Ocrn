3480e5960c857e3b41eb32f0997da4a091e02633
Vipin Nair <swvist@gmail.com>
2011-11-12 22:34:32 +0530

    Added loadTrainingData and teach methods

--- ocrn/neuralnet.py
+++ ocrn/neuralnet.py
@@ -1,15 +1,28 @@
 from pybrain.tools.shortcuts import buildNetwork
-import dataset
+from pybrain.supervised.trainers import BackpropTrainer
 
 class neuralnet:
 	def __init__(self, *args):
 		if len(args) >=2 :
 			self.nnet = buildNetwork(*args)
-			self.inputlayer = args[0]
-			self.outputlayer = args[len(args)-1]
+			self.inputdimension = args[0]
+			self.outputdimension = args[len(args)-1]
 			self.nnet.sortModules()
 			
 			print self.nnet
 		else:
 			print "Number of layers must be more than two\n"
+
+	def loadTrainingData(self, trainingdataset):
+		if trainingdataset.getDimension('input') == self.inputdimension and \
+			trainingdataset.getDimension('target') == self.outputdimension:
+				self.trainer = BackpropTrainer(self.nnet, trainingdataset)
+				return 1
+		else:
+			print "Dataset & Network size mismatch"
+			return 0
+
+	def teach(self, repeat):
+		for i in range (1, repeat+1):
+			print self.trainer.train()
 
