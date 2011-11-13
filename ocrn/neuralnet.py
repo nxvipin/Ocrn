6ae64e821030e7c3e8163442532f1f48bee866e5
Vipin Nair <swvist@gmail.com>
2011-11-13 22:19:48 +0530

    Added code to activate the network, added documnetation.

--- ocrn/neuralnet.py
+++ ocrn/neuralnet.py
@@ -11,18 +11,29 @@ class neuralnet:
 			
 			print self.nnet
 		else:
-			print "Number of layers must be more than two\n"
+			print "Number of layers must be greater than or equal to two\n"
 
+	# Loads the training data into neural network before training.
 	def loadTrainingData(self, trainingdataset):
 		if trainingdataset.getDimension('input') == self.inputdimension and \
 			trainingdataset.getDimension('target') == self.outputdimension:
 				self.trainer = BackpropTrainer(self.nnet, trainingdataset)
 				return 1
 		else:
-			print "Dataset & Network size mismatch"
+			print "Dataset-Network size mismatch\n"
 			return 0
 
-	def teach(self, repeat):
-		for i in range (1, repeat+1):
+	# Train the neural network 'n' times with loaded training data.
+	def teach(self, n):
+		for i in range (1, n+1):
 			print self.trainer.train()
+
+	# Activate the Neural Network with test data. Returns calculated output.
+	def activate(self, testdata):
+		if testdata.size == self.inputdimension:
+			return self.nnet.acivate(testdata)
+		else:
+			print "Test data error\n"
+			return 0
+
 
